c6ab0bb2d8c4646e9e9d1709bee0bcd4daffabfb
rutgerkool <rutgerkool@gmail.com>
2022-09-30 17:02:55 +0200

    Added filter bar

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -2,33 +2,46 @@ import React from 'react'
 import { CharacterProps } from './CharacterList'
 import './Character.css'
 import { useParams } from 'react-router-dom'
+import { Button } from '@mui/material'
 
 export function CharacterPage(props : {characters: CharacterProps[]}) {
     const params = useParams();
     const character = props.characters.find(el => el.id === Number(params.charId))!
+    
     return(
-        <div className='character-page' style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',        
-        }}>
-                <div>
-                    <img src={character.image} alt={character.name}/>
-                </div> 
-                <div style={{
-                    margin: 10,
-                    padding: 10,
-                }}>
-                    <h1>{character.name}</h1>
-                    <p><span className={character.status === 'Alive' ? 'status-dot-alive' : 
-                    (character.status === 'Dead' ? 'status-dot-dead' : 'status-dot-unknown')}></span>  {character.status} - {character.species}</p>
-                    
-                    <p>Last known location: </p>
-                    <p>{character.location.name}</p>
+        <div>
+            <div className='character-page' style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',        
+            }}>
+                    <div>
+                        <img src={character.image} alt={character.name}/>
+                    </div> 
+                    <div style={{
+                        margin: 10,
+                        padding: 10,
+                    }}>
+                        <h1>{character.name}</h1>
+                        <p><span className={character.status === 'Alive' ? 'status-dot-alive' : 
+                        (character.status === 'Dead' ? 'status-dot-dead' : 'status-dot-unknown')}></span>  {character.status} - {character.species}</p>
+                        
+                        <p>Last known location: </p>
+                        <p>{character.location.name}</p>
 
-                    <p>First seen in: </p>
-                    
-                </div>
+                        <p>First seen in: </p>
+                        
+                        <p>Gender: {character.gender}</p>
+
+                        {character.type ? <p>Type: {character.type}</p> : null}
+                    </div>
+                    <div>
+                        <Button variant='contained' href='/' >Back</Button>
+                    </div>
+            </div>
+            <div className='created-text'>
+                <p>Created: {character.created}</p>
+            </div>
         </div>
     )
 }
--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from 'react'
 import { Character } from './Character'
 import { Link } from 'react-router-dom'
 import './CharacterList.css'
+import { Button, ButtonGroup } from '@mui/material'
 
 export type CharacterProps = {
     id: number, 
@@ -14,10 +15,14 @@ export type CharacterProps = {
     location : {name: string, url: string};
     image : string;
     episodes : string[];
+    created: string;
 }
 
 export function CharacterList() {
     const [characters, setCharacters] = useState<CharacterProps[]>([]);
+    const [filterValue, setFilterValue] = useState<string>('');
+
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
     useEffect(() => {
         const url='https://rickandmortyapi.com/api/character/';
@@ -48,21 +53,33 @@ export function CharacterList() {
     return (
         <div>
             <img className='logo' src={require('./Rick-And-Morty-Logo.png')} alt={'logo'}/>
+            <div className='filter-bar'>
+                <ButtonGroup variant='contained' aria-label='outlined primary button group'>
+                    {alphabet.map(el => <Button onClick={e => {
+                        e.preventDefault();
+                        setFilterValue(el.toUpperCase());
+                    }}>{el}</Button>)}
+                </ButtonGroup>
+            </div>
             {list ? list.map(el => {
+            if (filterValue !== '' && el[0].name[0] !== filterValue) {
+                console.log(filterValue, 'and', el[0].name)
+                return null;}
             return (
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-                }}>
-                    <Link className='navlink' to={`/${el[0].id}`}>
-                        <Character {...el[0]}></Character>
-                    </Link>
-
-                    <Link className='navlink' to={`/${el[1].id}`}>
-                        <Character {...el[1]}></Character>
-                    </Link>
-                </div>
-                )})
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    }}>
+                        <Link className='navlink' to={`/${el[0].id}`}>
+                            <Character {...el[0]}></Character>
+                        </Link>
+    
+                        <Link className='navlink' to={`/${el[1].id}`}>
+                            <Character {...el[1]}></Character>
+                        </Link>
+                    </div>
+                    )
+            })
                  : null}
         </div>
     )
